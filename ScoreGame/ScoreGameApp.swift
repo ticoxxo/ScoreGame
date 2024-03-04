507bfbf029703c46807079c5892d57dc4a7a7456
Ticoxxo <ticoxxo@gmail.com>
2024-03-04 10:30:03 -0700

    Initial Commit

---
+++ ScoreGame/ScoreGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreGameApp.swift
+//  ScoreGame
+//
+//  Created by Alberto Almeida on 04/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ScoreGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
